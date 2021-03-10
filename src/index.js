94ca34daf9ab42facacb14cf72d36b57376a83c1
vakulinina <lerrun@gmail.com>
2021-03-10 21:06:11 +0300

    feat: add brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const map = {};
+
+  bracketsConfig.forEach(([open, close]) => {
+    map[open] = close;
+  })
+
+  for (let bracket of str) {
+    if (!map[bracket]) {
+      if (bracket !== map[stack.pop()]) return false;
+    } else {
+      if (map[bracket] === bracket) {
+        if (stack.includes(bracket)) {
+          if (bracket !== map[stack.pop()]) return false;
+        } else {
+          stack.push(bracket);
+        }
+      } else {
+        stack.push(bracket);
+      }
+    }
+  }
+
+  if (stack.length !== 0) return false;
+
+  return true;
 }
